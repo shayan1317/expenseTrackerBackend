551e06bc673878e70fdf55eafde8c7699c7dfc1a
shayan <shayanakhtar14@gmail.com>
2025-08-17 20:42:20 +0500

    chnages made

--- api/index.ts
+++ api/index.ts
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: ["http://localhost:5173/", "http://127.0.0.1:5173/"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: [
       "Origin",
