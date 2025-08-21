16469730e2b6f953e3811d6c4a1afc4d412863ba
shayan <shayanakhtar14@gmail.com>
2025-08-21 14:00:25 +0500

    backend done

--- server.ts
+++ server.ts
@@ -15,7 +15,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: [
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
+      "https://expense-tracker-backend-snowy-rho.vercel.app/api",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: [
       "Origin",
